8fe6287a75a55b0003ce68a8b2e91b60bc5c8602
RAJATH <RAJATH@172.19.151.19>
2019-12-09 20:59:53 +0100

    Edit contact number alert boxes

---
+++ src/verticalMenuPersonalInfo/EditEmergencyContact.java
@@ -0,0 +1,35 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EditEmergencyContact {
+	
+	@FXML
+    private JFXButton No_Emergency_Contact;
+
+    @FXML
+    private JFXButton Yes_Emergency_Contact;
+
+    @FXML
+    private JFXButton CLOSE;
+
+    @FXML
+    void CloseWindow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditEmergencyContact(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RedirectToEmployee(ActionEvent event) {
+
+    }
+
+    
+}
---
+++ src/verticalMenuPersonalInfo/PersonalContactEdit.java
@@ -0,0 +1,35 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PersonalContactEdit {
+	
+	 @FXML
+	    private JFXButton No_Personal_Contact;
+
+	    @FXML
+	    private JFXButton Yes_Personal_Contact;
+
+	    @FXML
+	    private JFXButton CLOSE;
+
+	    @FXML
+	    void CloseWindow(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void EditPersonalContact(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void RedirectToEmergencyNumber(ActionEvent event) {
+
+	    }
+
+
+}
