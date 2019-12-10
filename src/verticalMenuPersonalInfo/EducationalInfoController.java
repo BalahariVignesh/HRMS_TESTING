01524702a241254038f1720915726f9b2823246d
RAJATH <RAJATH@172.19.151.19>
2019-12-10 18:06:20 +0100

    Education UI And changes

---
+++ src/verticalMenuPersonalInfo/EducationalInfoController.java
@@ -0,0 +1,34 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class EducationalInfoController {
+	@FXML
+    private JFXTextField HighestE;
+
+    @FXML
+    private JFXTextArea EducationalDetails;
+
+    @FXML
+    private JFXButton Cancel_btn;
+
+    @FXML
+    private JFXButton Edit_Education_Info;
+
+    @FXML
+    void CancelEinfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditEducationInfo(ActionEvent event) {
+
+    }
+
+}
---
+++ src/verticalMenuPersonalInfo/EducationalInfoEditController.java
@@ -0,0 +1,41 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EducationalInfoEditController {
+	 @FXML
+	    private JFXTextField HighestEducationEdit;
+
+	    @FXML
+	    private JFXTextArea EditEducationDetails;
+
+	    @FXML
+	    private JFXButton Cancel_Cancel_btn;
+
+	    @FXML
+	    private JFXButton SAVE_Education_Info;
+
+	    @FXML
+	    private JFXButton CLOSE;
+
+	    @FXML
+	    void CancelCinfo(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void CloseWindow(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void DisplayEducationEdit(ActionEvent event) {
+
+	    }
+
+}
