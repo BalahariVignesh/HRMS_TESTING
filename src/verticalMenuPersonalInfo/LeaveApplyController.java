1ce6dcdd43b8ce55b0a8cd85ff60fea61ed1685c
RAJATH <RAJATH@172.19.151.19>
2019-12-16 11:38:15 +0100

    Leave GUI and Controllers

---
+++ src/verticalMenuPersonalInfo/LeaveApplyController.java
@@ -0,0 +1,31 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LeaveApplyController {
+	 @FXML
+	    private JFXRadioButton SickLeave;
+
+	    @FXML
+	    private JFXRadioButton CasualLeave;
+
+	    @FXML
+	    private JFXButton SavePermanentAddress;
+
+	    @FXML
+	    private JFXButton CLOSE;
+
+	    @FXML
+	    void CloseWindow(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void DisplayPermanentAddress(ActionEvent event) {
+
+	    }
+}
---
+++ src/verticalMenuPersonalInfo/LeaveInfoController.java
@@ -0,0 +1,23 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LeaveInfoController {
+    @FXML
+    private JFXTextField CasualLeave;
+
+    @FXML
+    private JFXTextField SickLeave;
+
+    @FXML
+    private JFXButton Request_Leave_Btn;
+
+    @FXML
+    void RequestNewLeave(ActionEvent event) {
+
+    }
+}
