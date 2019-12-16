1810a9b3892d060e57299c097ccbd21c781110b0
RAJATH <RAJATH@172.19.151.19>
2019-12-16 11:01:22 +0100

    Assets GUI and Controller

---
+++ src/verticalMenuPersonalInfo/AssetsAddNewController.java
@@ -0,0 +1,26 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AssetsAddNewController {
+	
+    @FXML
+    private JFXButton SavePermanentAddress;
+
+    @FXML
+    private JFXButton CLOSE;
+
+    @FXML
+    void CloseWindow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DisplayPermanentAddress(ActionEvent event) {
+
+    }
+    
+}
---
+++ src/verticalMenuPersonalInfo/AssetsInfoController.java
@@ -0,0 +1,22 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AssetsInfoController {
+	
+    @FXML
+    private JFXTextArea AssetsOwned;
+
+    @FXML
+    private JFXButton Request_New_Btn;
+
+    @FXML
+    void RequestNewAsset(ActionEvent event) {
+
+    }
+
+}
