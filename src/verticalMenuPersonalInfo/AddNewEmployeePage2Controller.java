58d7b9bf5e3e8d452c97c8274fb915419da94920
RAJATH <RAJATH@172.19.151.19>
2019-12-17 20:33:33 +0100

    AddNewEmployee Page Additional

---
+++ src/verticalMenuPersonalInfo/AddNewEmployeePage2Controller.java
@@ -0,0 +1,57 @@
+package verticalMenuPersonalInfo;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class AddNewEmployeePage2Controller {
+    @FXML
+    private JFXButton CLOSE;
+
+    @FXML
+    private JFXTextField NewAddressLine;
+
+    @FXML
+    private JFXTextField NewStreet;
+
+    @FXML
+    private JFXTextField NewCity;
+
+    @FXML
+    private JFXTextField NewZip;
+
+    @FXML
+    private JFXTextField NewOfficeContact;
+
+    @FXML
+    private JFXTextField NewOfficeEmail;
+
+    @FXML
+    private JFXTextField NewLevel;
+
+    @FXML
+    private JFXTextField NewBranch;
+
+    @FXML
+    private JFXTextField NewStatus;
+
+    @FXML
+    private JFXButton NewEmployeeData;
+
+    @FXML
+    void AddNewEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CloseWindow(ActionEvent event) {
+        // get a handle to the stage
+        Stage stage = (Stage) CLOSE.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+
+}
