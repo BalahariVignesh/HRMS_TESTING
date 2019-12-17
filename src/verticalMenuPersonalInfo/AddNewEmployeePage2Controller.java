b5bd005568f1ab1c5ae9ebef07b3ec927fbce31f
RAJATH <RAJATH@172.19.151.19>
2019-12-17 20:38:15 +0100

    Addition of radio button in add new employee2

--- src/verticalMenuPersonalInfo/AddNewEmployeePage2Controller.java
+++ src/verticalMenuPersonalInfo/AddNewEmployeePage2Controller.java
@@ -1,6 +1,7 @@
 package verticalMenuPersonalInfo;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 
 import javafx.event.ActionEvent;
@@ -8,43 +9,55 @@ import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
 public class AddNewEmployeePage2Controller {
-    @FXML
-    private JFXButton CLOSE;
+	 @FXML
+	    private JFXButton CLOSE;
 
-    @FXML
-    private JFXTextField NewAddressLine;
+	    @FXML
+	    private JFXTextField NewAddressLine;
 
-    @FXML
-    private JFXTextField NewStreet;
+	    @FXML
+	    private JFXTextField NewStreet;
 
-    @FXML
-    private JFXTextField NewCity;
+	    @FXML
+	    private JFXTextField NewCity;
 
-    @FXML
-    private JFXTextField NewZip;
+	    @FXML
+	    private JFXTextField NewZip;
 
-    @FXML
-    private JFXTextField NewOfficeContact;
+	    @FXML
+	    private JFXTextField NewOfficeContact;
 
-    @FXML
-    private JFXTextField NewOfficeEmail;
+	    @FXML
+	    private JFXTextField NewOfficeEmail;
 
-    @FXML
-    private JFXTextField NewLevel;
+	    @FXML
+	    private JFXTextField NewLevel;
 
-    @FXML
-    private JFXTextField NewBranch;
+	    @FXML
+	    private JFXTextField NewBranch;
 
-    @FXML
-    private JFXTextField NewStatus;
+	    @FXML
+	    private JFXTextField NewStatus;
 
-    @FXML
-    private JFXButton NewEmployeeData;
+	    @FXML
+	    private JFXRadioButton HR;
 
-    @FXML
-    void AddNewEmployee(ActionEvent event) {
+	    @FXML
+	    private JFXRadioButton EMPLOYEE;
 
-    }
+	    @FXML
+	    private JFXRadioButton MANAGER;
+
+	    @FXML
+	    private JFXRadioButton PMO;
+
+	    @FXML
+	    private JFXButton NewEmployeeData;
+
+	    @FXML
+	    void AddNewEmployee(ActionEvent event) {
+
+	    }
 
     @FXML
     void CloseWindow(ActionEvent event) {
